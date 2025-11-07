fc21f5aea837b8c11983dee0edd5b6f331c718a0
Ilya-Bugaev <bugaevi07@mail.ru>
2025-11-07 16:07:15 +0300

    correction of module names in tests

--- test/test_checksum.py
+++ test/test_checksum.py
@@ -2,8 +2,8 @@ from src.checksum import modulo11_checksum
 
 
 def test_good():
-    assert modulo11Checksum("2-266-11156-8")
+    assert modulo11_checksum("2-266-11156-8")
 
 
 def test_bad():
-    assert not modulo11Checksum("2-266-11156-3")
+    assert not modulo11_checksum("2-266-11156-3")
